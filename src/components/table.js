5a7ce14122787e68ccd148b40479f2cb758f5cf2
Irina Fominykh <irina.fmn96@yandex.ru>
2025-11-22 00:18:33 +0200

    dev

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,18 +8,45 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  const renderAdditional = (action, data) => {
+    data.forEach((subName) => {
+      root[subName] = cloneTemplate(subName);
+      root.container[action](root[subName].container);
+    });
+  };
+  renderAdditional("prepend", [...before].reverse());
+  renderAdditional("append", after);
+  
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+  root.container.addEventListener("change", () => {
+    onAction();
+  });
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction);
+  });
+  root.container.addEventListener("submit", (e) => {
+    e.defaultPrevented();
+    onAction(e.submitter);
+  });
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+      Object.keys(item)
+        .filter((key) => key in row.elements)
+        .forEach((key) => {
+          row.elements[key].textContent = item[key];
+        });
+      return row.container;
+    });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
 
-    return {...root, render};
+  return { ...root, render };
 }
